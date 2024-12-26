004a3ccd5bccb5217fdae8cc30b98e3db2da168d
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2024-12-26 23:03:06 +0800

    fix: google test fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
           title: '2025跨年倒數計時',
           meta: [
               { name: 'theme-color', content: '#f43f5e' },
+              { name: 'google-site-verification', content: 'ztKMEYWYv5oRbWiHxUCdgPTbXqBKW5e5mshnU-IbM7w' },
               { property: 'og:title', content: '2025跨年倒數計時' },
               { property: 'og:url', content: 'https://newyear-firework.vercel.app/' },
               { property: 'og:image', content: 'https://newyear-firework.vercel.app/taipei101.jpg' },
