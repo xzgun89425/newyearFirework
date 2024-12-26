ada782bb995f6073bd5f074135ae861d9065e72a
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2024-12-26 21:32:53 +0800

    fix:og img

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
             { name: 'theme-color', content: '#f43f5e' },
             { property: 'og:title', content: '2025跨年煙火倒數' },
             { property: 'og:url', content: 'https://https://newyear-firework.vercel.app/' },
-            { property: 'og:image', content: 'https://https://newyear-firework.vercel.app//taipei101.jpg' },
+            { property: 'og:image', content: 'https://https://newyear-firework.vercel.app/taipei101.jpg' },
             { name: 'description', content: '2025跨年煙火倒數，2025，臺中跨年煙火、臺北跨年煙火、高雄跨年煙火、阿里山跨年煙火' },
             {
                 property: 'og:description',
