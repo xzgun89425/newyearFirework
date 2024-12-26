81fdaa5b066a545b8a4a2cf7b162a94cce0e8316
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2024-12-26 23:11:36 +0800

    fix: change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,20 +6,20 @@ export default defineNuxtConfig({
   app: {
       head: {
           viewport: 'width=device-width, initial-scale=1',
-          title: '2025跨年倒數計時',
+          title: '跨年倒數計時',
           meta: [
               { name: 'theme-color', content: '#f43f5e' },
               { name: 'google-site-verification', content: 'ztKMEYWYv5oRbWiHxUCdgPTbXqBKW5e5mshnU-IbM7w' },
-              { property: 'og:title', content: '2025跨年倒數計時' },
+              { property: 'og:title', content: '跨年倒數計時' },
               { property: 'og:url', content: 'https://newyear-firework.vercel.app/' },
               { property: 'og:image', content: 'https://newyear-firework.vercel.app/taipei101.jpg' },
               {
                   name: 'description',
-                  content: '2025跨年倒數計時，2025，臺中跨年煙火、臺北跨年煙火、高雄跨年煙火、阿里山跨年煙火',
+                  content: '跨年倒數計時，2025，臺中跨年煙火、臺北跨年煙火、高雄跨年煙火、阿里山跨年煙火',
               },
               {
                   property: 'og:description',
-                  content: '2025跨年倒數計時，2025，臺中跨年煙火、臺北跨年煙火、高雄跨年煙火、阿里山跨年煙火',
+                  content: '跨年倒數計時，2025，臺中跨年煙火、臺北跨年煙火、高雄跨年煙火、阿里山跨年煙火',
               },
           ],
       },
